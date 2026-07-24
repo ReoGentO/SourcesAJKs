02e7c48ea6d2bcc5aadb40f46a442614917d8a93
ReoGentO <104772613+ReoGentO@users.noreply.github.com>
2026-07-24 23:40:58 +0500

    Change button text indicators from checkmarks to brackets

--- source.lua
+++ source.lua
@@ -686,7 +686,7 @@ function library:Window(name)
 	        Button.BorderColor3 = Color3.fromRGB(113,128,147)
 	        Button.Position = UDim2.new(0,6,0,canvas)
 	        Button.Size = UDim2.new(0,170,0,26)
-	        Button.Text = (states[name] and "✓ " or "✗ ") .. name
+	        Button.Text = (states[name] and "[x] " or "[ ] ") .. name
 	        Button.Font = Enum.Font.SourceSans
 	        Button.TextColor3 = Color3.fromRGB(245,246,250)
 	        Button.TextSize = 16
@@ -709,7 +709,7 @@ function library:Window(name)
 	
 	        Button.MouseButton1Up:Connect(function()
 	            states[name] = not states[name]
-				Button.Text = (states[name] and "✓ " or "✗ ") .. name
+				Button.Text = (states[name] and "[x] " or "[ ] ") .. name
 				callback(name, states[name])
 	        end)
 	
